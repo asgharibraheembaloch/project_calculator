dd758a12f041cc0d60524ad2d48ebfc29f66670a
Asghar Ibraheem Baloch <46741864+asgharibraheembaloch@users.noreply.github.com>
2022-10-18 02:12:20 +0500

    updated index.tx file

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,76 @@
 import inquirer from 'inquirer';
-import * as fs from 'fs';
 
-const CHOICES = fs.readdirSync(`./templates`);
+async function calculator() {
+  const calculation = await inquirer.prompt([
+    {type:"list", name:"type", choices:["add", "subtract", "multiply", "divide"]}
+  ])
+  const input1 = await inquirer.prompt([
+    {type: "number", name:"input", message:"please input number value"}
+  ])
+  const input2 = await inquirer.prompt([
+    {type: "number", name:"input", message:"please input number value"}
+  ])
+  if(calculation.type==='add'){
+    let  val = 0
+    let flag = false
+
+    /* let input = await inquirer.prompt([
+      {type: "number", name:"input", message:"please input number value"}
+    ])
+
+    add += input */
+    
+    if(!flag){
+      let confirmed = await inquirer.prompt([
+        {type: "confirm", name:"(y/N)", message:"if you have completed all calculation please select y for yes else select n for No"}
+      ])
+      if (confirmed['(y/N)']){
+        flag = true
+      } else
+      {
+        flag = false
+      }
+    } else {
+      let input = await inquirer.prompt([
+        {type: "number", name:"input", message:"please input number value"}
+      ])
+      val += input['input']
+    }
+
+  } else if(calculation.type==='subtract'){
+    console.log(input1['input'] - input2['input'])
+  } else if(calculation.type==='multiply'){
+    console.log(input1['input'] * input2['input'])
+  } else if(calculation.type==='divide'){
+    console.log(input1['input'] / input2['input'])
+  }
+}
+
+calculator()
+
+/* const CHOICES = ["add", "sutract", "multiply", "divide" ];
 
 const QUESTIONS = [
   {
-    name: 'project-choice',
+    name: 'Caculation Type',
     type: 'list',
-    message: 'What project template would you like to generate?',
+    message: 'please select type of caculation you want to proceed',
     choices: CHOICES
   },
   {
-    name: 'project-name',
+    name: 'General Purpose',
     type: 'input',
-    message: 'Project name:',
-    validate: function (input: string) {
-      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
-    }
+    message: 'input number value:',
+  },
+  {
+    name: 'General Purpose',
+    type: 'input',
+    message: 'input number value:',
   }
 ];
 
 
 inquirer.prompt(QUESTIONS)
-  .then((answers: string) => {
+  .then((answers: number) => {
     console.log(answers);
-});
+}); */
