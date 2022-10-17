e4a1b90d2b954ba755810721ce229f66dc7762fd
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-17 15:49:08 +0500

    created initial template for the project

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import inquirer from 'inquirer';
+import * as fs from 'fs';
+
+const CHOICES = fs.readdirSync(`./templates`);
+
+const QUESTIONS = [
+  {
+    name: 'project-choice',
+    type: 'list',
+    message: 'What project template would you like to generate?',
+    choices: CHOICES
+  },
+  {
+    name: 'project-name',
+    type: 'input',
+    message: 'Project name:',
+    validate: function (input: string) {
+      if (/^([A-Za-z\-\_\d])+$/.test(input)) return true;
+      else return 'Project name may only include letters, numbers, underscores and hashes.';
+    }
+  }
+];
+
+
+inquirer.prompt(QUESTIONS)
+  .then((answers: string) => {
+    console.log(answers);
+});
