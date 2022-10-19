79ffa84a7f97a9e126f51cfb88e5ad5cf38f0e88
Asghar Ibraheem Baloch <46741864+asgharibraheembaloch@users.noreply.github.com>
2022-10-20 01:27:55 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,44 @@
 import inquirer from 'inquirer';
 
 async function calculator() {
-  const calculation = await inquirer.prompt([
-    {type:"list", name:"type", choices:["add", "subtract", "multiply", "divide"]}
-  ])
-  const input1 = await inquirer.prompt([
-    {type: "number", name:"input", message:"please input number value"}
-  ])
-  const input2 = await inquirer.prompt([
-    {type: "number", name:"input", message:"please input number value"}
-  ])
-
+  let count = 0
+  let val = 0
   let calc_done = false
 
+  const calculation = await inquirer.prompt([
+    { type: "list", name: "type", choices: ["add", "subtract", "multiply", "divide"] }
+  ])
 
   while (!calc_done) {
-    let val = 0
-    let confirmed = await inquirer.prompt([
-      {type: "confirm", name:"(y/N)", message:"if you have completed all calculation please select y for yes else select n for No"}
+
+    let input = await inquirer.prompt([
+      { type: "number", name: "input", default: 0, message: "please input number value" }
     ])
-    if (confirmed['(y/N)']){
-      calc_done = true
-    } else {
-      let input = await inquirer.prompt([
-        {type: "number", name:"input",default:0, message:"please input number value"}
-      ])
-      if(calculation.type==='add'){
-        val += input['input']
-      } else if(calculation.type==='subtract'){
-        val -= input['input']
-      } else if(calculation.type==='multiply'){
-        val *= input['input']
-      } else if(calculation.type==='divide'){
-        (input1['input'] / input2['input'])
-        calc_done = true
-      }
-      val += input['input']
-      calc_done = false
+
+    count += 1
+
+    if (calculation.type === 'add') {
+      val += +input['input']
+    } else if (calculation.type === 'subtract') {
+      val -= input['input']
+    } else if (calculation.type === 'multiply') {
+      val *=  input['input']
+    } else if (calculation.type === 'divide') {
+      val /=  input['input']
     }
-  }  
+    console.log(count)
+    if (count >= 2) {
+      let confirmed = await inquirer.prompt([
+        { type: "confirm", name: "(y/N)", message: "if you have completed all calculation please select y for yes else select n for No" }
+      ])
+      if (confirmed['(y/N)']) {
+        calc_done = true
+      } else {
+        false
+      }
+    }
+  }
+  console.log(val)
 }
 
 calculator()
