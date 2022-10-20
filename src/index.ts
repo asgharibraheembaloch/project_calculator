40dfa118742d75e5763eedc201a6db636855a542
Asghar Ibraheem Baloch <asgharibraheembaloch@gmail.com>
2022-10-20 14:57:44 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import inquirer from 'inquirer';
 
 async function calculator() {
-  let count = 0
-  let val = 0
+  let count = 0;
+  let total = 0;
+  let values:number[] = [];
   let calc_done = false
 
   const calculation = await inquirer.prompt([
@@ -16,29 +17,35 @@ async function calculator() {
     ])
 
     count += 1
-
-    if (calculation.type === 'add') {
-      val += +input['input']
-    } else if (calculation.type === 'subtract') {
-      val -= input['input']
-    } else if (calculation.type === 'multiply') {
-      val *=  input['input']
-    } else if (calculation.type === 'divide') {
-      val /=  input['input']
-    }
+    values.push(input['input'])
+    
     console.log(count)
     if (count >= 2) {
       let confirmed = await inquirer.prompt([
         { type: "confirm", name: "(y/N)", message: "if you have completed all calculation please select y for yes else select n for No" }
       ])
       if (confirmed['(y/N)']) {
+        switch (calculation.type) {
+          case 'add':
+            total = values.reduce((total, val)=>total+val)
+            break;        
+          case 'subtract':
+            total = values.reduce((total, val)=>total-val)
+            break;
+          case 'multiply':
+            total = values.reduce((total, val)=>total*val)
+            break;
+          case 'divide':
+            total = values.reduce((total, val)=>total/val)
+            break
+        }
         calc_done = true
       } else {
         false
       }
     }
   }
-  console.log(val)
+  console.log(total)
 }
 
 calculator()
